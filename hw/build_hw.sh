ab2a27684d9f70e905547210e4d8b679059c148e
MorriganR <dasha@ddyka.com>
2021-10-26 23:11:17 +0200

    fix Critical Warning (332012): Synopsys Design Constraints File file not found

--- hw/build_hw.sh
+++ hw/build_hw.sh
@@ -9,8 +9,8 @@ ls -la
 qsys-generate soc_system.qsys --synthesis=VERILOG --part=5CSEBA6U23I7 || { echo "Generate RTL from soc_system.qsys - ERROR" ; exit 1 ; }
 
 # Compile RTL design
-quartus_sh --flow compile DE10-Nano-JTAG || { echo "Compile RTL design - ERROR" ; exit 1 ; }
+quartus_sh --flow compile DE10_NANO_JTAG || { echo "Compile RTL design - ERROR" ; exit 1 ; }
 
 # Generate RBF file
 cd output_files
-quartus_cpf -c -o bitstream_compression=on DE10-Nano-JTAG.sof DE10-Nano-JTAG.rbf || { echo "Generate RBF file - ERROR" ; exit 1 ; }
+quartus_cpf -c -o bitstream_compression=on DE10_NANO_JTAG.sof DE10_NANO_JTAG.rbf || { echo "Generate RBF file - ERROR" ; exit 1 ; }
