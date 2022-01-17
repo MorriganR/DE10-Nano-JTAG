28481b17534751a22e141ac7877f9aab60a6279c
MorriganR <dasha@ddyka.com>
2022-01-17 13:37:32 +0200

    fix tb & gitignore

--- hw/tb/ltc2308_tb.v
+++ hw/tb/ltc2308_tb.v
@@ -7,15 +7,18 @@ reg adc_sck = 1'b0;
 reg adc_sdi = 1'b0;
 wire adc_sdo;
 
-ltc2308 #(
-  .tWHCONV_MIN( `tWHCONV_MIN ),
-  .tWHCONV_MAX( `tWHCONV_MAX )
-) ltc2308 (
+ltc2308 ltc2308 (
   .adc_convst( adc_convst ),//input adc_convst,
   .adc_sck( adc_sck ),//input adc_sck,
   .adc_sdi( adc_sdi ),//input adc_sdi,
   .adc_sdo( adc_sdo )//output adc_sdo
 );
+`ifdef tWHCONV_MIN
+  defparam ltc2308.tWHCONV_MIN = `tWHCONV_MIN;
+`endif
+`ifdef tWHCONV_MAX
+  defparam ltc2308.tWHCONV_MAX = `tWHCONV_MAX;
+`endif
 
 initial begin
   adc_convst = 1'b0;
