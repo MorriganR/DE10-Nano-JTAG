e227fa5a4a104ac5208f5feb4855465898274fb8
MorriganR <dasha@ddyka.com>
2022-01-18 15:08:27 +0200

    adc tb: add debug port

--- hw/tb/ltc2308.v
+++ hw/tb/ltc2308.v
@@ -4,7 +4,7 @@ module ltc2308 #(
 // SYMBOL     | PARAMETER                             | MIN TYP MAX | UNITS
 // fSMPL(MAX) | Maximum Sampling Frequency            |         500 | kHz
 // fSCK       | Shift Clock Frequency                 |         40  | MHz
-parameter tWCLK = 25_000,
+parameter tWCLK_MIN = 25_000,
 // tWHCONV    | CONVST High Time                      | 20          | ns
 parameter tWHCONV_MIN = 20_000,
 parameter tWHCONV_MAX = 40_000,
@@ -17,11 +17,15 @@ parameter tWLCLK = 10_000,
 // tWLCONVST  | CONVST Low Time During Data Transfer  | 410         | ns
 // tHCONVST   | Hold Time CONVST Low After Last SCK↓  | 20          | ns
 // tCONV      | Conversion Time                       |     1.3 1.6 | µs
+parameter tCONV_TYP = 1_300_000,
 parameter tCONV_MAX = 1_600_000,
 // tACQ       | Acquisition Time 7th SCK↑ to CONVST↑  | 240         | ns
 // tREFWAKE   | REFCOMP Wakeup Time                   |    200      | ms
 // tdDO       | SDO Data Valid After SCK↓             |    10.8 12.5| ns
+parameter tdDO_TYP = 10_800,
+parameter tdDO_MAX = 12_500,
 // thDO       | SDO Hold Time After SCK↓              | 4           | ns
+parameter thDO_MIN = 4_000,
 // ten        | SDO Valid After CONVST↓               |     11 15   | ns
 // tdis       | Bus Relinquish Time                   |     11 15   | ns
 // tr         | SDO Rise Time                         |     4       | ns
@@ -38,55 +42,70 @@ bit decision) or after the conversion ends (tCONV).
   input adc_convst,
   input adc_sck,
   input adc_sdi,
-  output adc_sdo
+  output adc_sdo,
+
+  input [11:0] dbg_in_adat_CH0,
+  input [11:0] dbg_in_adat_CH1,
+  input [11:0] dbg_in_adat_CH2,
+  input [11:0] dbg_in_adat_CH3,
+  input [11:0] dbg_in_adat_CH4,
+  input [11:0] dbg_in_adat_CH5,
+  input [11:0] dbg_in_adat_CH6,
+  input [11:0] dbg_in_adat_CH7
 );
+
+reg adc_sdo = 1'bz;
 
 initial
   $timeformat(-9, 3, "ns", 0);
-
-
 
 // adc_convst
 time adc_convst_tpos = 0;
 time adc_convst_tneg = 0;
 event adc_convst_p;
 event adc_convst_n;
-always @( posedge adc_convst ) begin
-  adc_convst_tpos <= $time;
-  -> adc_convst_p;
-end
-always @( negedge adc_convst ) begin
-  adc_convst_tneg <= $time;
-  -> adc_convst_n;
-end
+always @( posedge adc_convst )
+  if ( $time != 0 ) begin
+    adc_convst_tpos <= $time;
+    -> adc_convst_p;
+  end
+always @( negedge adc_convst )
+  if ( $time != 0 ) begin
+    adc_convst_tneg <= $time;
+    -> adc_convst_n;
+  end
 
 // adc_sck
 time adc_sck_tpos = 0;
 time adc_sck_tneg = 0;
 event adc_sck_p;
 event adc_sck_n;
-always @( posedge adc_sck ) begin
-  adc_sck_tpos <= $time;
-  -> adc_sck_p;
-end
-always @( negedge adc_sck ) begin
-  adc_sck_tneg <= $time;
-  -> adc_sck_n;
-end
+always @( posedge adc_sck )
+  if ( $time != 0 ) begin
+    adc_sck_tpos <= $time;
+    -> adc_sck_p;
+  end
+always @( negedge adc_sck )
+  if ( $time != 0 ) begin
+    adc_sck_tneg <= $time;
+    -> adc_sck_n;
+  end
 
 // adc_sdi
 time adc_sdi_tpos = 0;
 time adc_sdi_tneg = 0;
 event adc_sdi_p;
 event adc_sdi_n;
-always @( posedge adc_sdi ) begin
-  adc_sdi_tpos <= $time;
-  -> adc_sdi_p;
-end
-always @( negedge adc_sdi ) begin
-  adc_sdi_tneg <= $time;
-  -> adc_sdi_n;
-end
+always @( posedge adc_sdi )
+  if ( $time != 0 ) begin
+    adc_sdi_tpos <= $time;
+    -> adc_sdi_p;
+  end
+always @( negedge adc_sdi )
+  if ( $time != 0 ) begin
+    adc_sdi_tneg <= $time;
+    -> adc_sdi_n;
+  end
 
 // checking tWHCONV
 always @( adc_convst_n ) begin
@@ -107,18 +126,18 @@ end
 // checking tWLCLK, tWCLK (adc_sck: pos to pos time)
 always @( adc_sck_p ) begin
   if ( ( adc_sck_tneg != 0 ) && ( adc_sck_tpos != 0 ) &&
-        ( ( $time - adc_sck_tneg < tWLCLK ) || ( $time - adc_sck_tpos < tWCLK ) ) ) begin
+        ( ( $time - adc_sck_tneg < tWLCLK ) || ( $time - adc_sck_tpos < tWCLK_MIN ) ) ) begin
     $display( "time = %t, tWLCLK = %t (should be more than %t), tWCLK = %t (should be more than %t)",
-        $time, $time - adc_sck_tneg, tWLCLK, $time - adc_sck_tpos, tWCLK );
+        $time, $time - adc_sck_tneg, tWLCLK, $time - adc_sck_tpos, tWCLK_MIN );
     $fatal(2);
   end
 end
 // checking tWHCLK, tWCLK (adc_sck: neg to neg time)
 always @( adc_sck_n ) begin
   if ( ( adc_sck_tneg != 0 ) && ( adc_sck_tpos != 0 ) &&
-        ( ( $time - adc_sck_tpos < tWHCLK ) || ( $time - adc_sck_tneg < tWCLK ) ) ) begin
+        ( ( $time - adc_sck_tpos < tWHCLK ) || ( $time - adc_sck_tneg < tWCLK_MIN ) ) ) begin
     $display( "time = %t, tWHCLK = %t (should be more than %t), tWCLK = %t (should be more than %t)",
-        $time, $time - adc_sck_tpos, tWHCLK, $time - adc_sck_tneg, tWCLK );
+        $time, $time - adc_sck_tpos, tWHCLK, $time - adc_sck_tneg, tWCLK_MIN );
     $fatal(2);
   end
 end
@@ -129,6 +148,56 @@ always @( adc_sck_p or adc_sck_n) begin
         $time, $time - adc_convst_tpos, tCONV_MAX );
     $fatal(2);
   end
+end
+
+/*
+[5] S/D = SINGLE-ENDED/DIFFERENTIAL BIT
+[4] O/S = ODD/SIGN BIT
+[3] S1  = ADDRESS SELECT BIT 1
+[2] S0  = ADDRESS SELECT BIT 0
+[1] UNI = UNIPOLAR/BIPOLAR BIT
+[0] SLP = SLEEP MODE BIT
+*/
+reg [5:0] d_in_word = 6'b1000_00;
+reg [11:0] dbg_in_adat_reg = 0;
+integer adc_sck_counter = 0;
+
+always @( adc_convst_p ) begin
+  case ( d_in_word[5:2] )
+    4'b0000 : dbg_in_adat_reg <= dbg_in_adat_CH0 - dbg_in_adat_CH1;
+    4'b0001 : dbg_in_adat_reg <= dbg_in_adat_CH2 - dbg_in_adat_CH3;
+    4'b0010 : dbg_in_adat_reg <= dbg_in_adat_CH4 - dbg_in_adat_CH5;
+    4'b0011 : dbg_in_adat_reg <= dbg_in_adat_CH6 - dbg_in_adat_CH7;
+    4'b0100 : dbg_in_adat_reg <= dbg_in_adat_CH1 - dbg_in_adat_CH0;
+    4'b0101 : dbg_in_adat_reg <= dbg_in_adat_CH3 - dbg_in_adat_CH2;
+    4'b0110 : dbg_in_adat_reg <= dbg_in_adat_CH5 - dbg_in_adat_CH4;
+    4'b0111 : dbg_in_adat_reg <= dbg_in_adat_CH7 - dbg_in_adat_CH6;
+    4'b1000 : dbg_in_adat_reg <= dbg_in_adat_CH0;
+    4'b1001 : dbg_in_adat_reg <= dbg_in_adat_CH2;
+    4'b1010 : dbg_in_adat_reg <= dbg_in_adat_CH4;
+    4'b1011 : dbg_in_adat_reg <= dbg_in_adat_CH6;
+    4'b1100 : dbg_in_adat_reg <= dbg_in_adat_CH1;
+    4'b1101 : dbg_in_adat_reg <= dbg_in_adat_CH3;
+    4'b1110 : dbg_in_adat_reg <= dbg_in_adat_CH5;
+    4'b1111 : dbg_in_adat_reg <= dbg_in_adat_CH7;
+    default: dbg_in_adat_reg <= dbg_in_adat_CH0;
+  endcase
+  adc_sck_counter <= 4'd0;
+  // d_in_word <= 6'b1000_00;
+  adc_sdo <= 1'bz;
+  #tCONV_TYP adc_sdo <= dbg_in_adat_reg[11];
+  dbg_in_adat_reg <= {dbg_in_adat_reg[10:0], 1'b0};
+end
+
+always @( adc_sck_p )
+  if ( adc_sck_counter < 6 )
+    d_in_word <= {d_in_word[4:0], adc_sdi};
+
+always @( adc_sck_n ) begin
+  adc_sck_counter = adc_sck_counter + 1;
+  #thDO_MIN adc_sdo <= 1'bx;
+  #( tdDO_MAX - thDO_MIN ) adc_sdo <= dbg_in_adat_reg[11];
+  dbg_in_adat_reg <= {dbg_in_adat_reg[10:0], 1'b0};
 end
 
 endmodule
--- hw/tb/ltc2308_tb_test.py
+++ hw/tb/ltc2308_tb_test.py
@@ -2,29 +2,54 @@ import unittest
 import os
 
 NOT_FATAL = 0
-vvp = "vvp ./tb/ltc2308_tb{}.vvp"
-iverilog = "iverilog {} -y./tb -o ./tb/ltc2308_tb{}.vvp ./tb/ltc2308_tb.v"
+vvp = "vvp ./tb/ltc2308_tb.vvp"
+iverilog = "iverilog {} -y./tb -o ./tb/ltc2308_tb.vvp ./tb/ltc2308_tb.v"
 
 class ltc2308(unittest.TestCase):
+  def test_tWCLK_MIN(self):
+    # in ltc2308_tb.v: tWCLK = 25_000
+
+    # tWCLK > tWCLK_MIN => test - PASS
+    self.assertEqual(os.system(iverilog.format("-D tWCLK_MIN=20_000")), NOT_FATAL)
+    self.assertEqual(os.system(vvp), NOT_FATAL)
+
+    # tWCLK == tWCLK_MIN => test - PASS
+    self.assertEqual(os.system(iverilog.format("-D tWCLK_MIN=25_000")), NOT_FATAL)
+    self.assertEqual(os.system(vvp), NOT_FATAL)
+
+    # tWCLK < tWCLK_MIN => test - FAIL
+    self.assertEqual(os.system(iverilog.format("-D tWCLK_MIN=25_001")), NOT_FATAL)
+    self.assertNotEqual(os.system(vvp), NOT_FATAL)
+
   def test_tWHCONV_MIN(self):
-    self.assertEqual(os.system(iverilog.format("-DtWHCONV_MIN=20_000", "0")), NOT_FATAL)
-    self.assertEqual(os.system(vvp.format("0")), NOT_FATAL)
+    # in ltc2308_tb.v: tWHCONV = 30_000
 
-    self.assertEqual(os.system(iverilog.format("-DtWHCONV_MIN=30_000", "1")), NOT_FATAL)
-    self.assertEqual(os.system(vvp.format("1")), NOT_FATAL)
+    # tWHCONV > tWHCONV_MIN => test - PASS
+    self.assertEqual(os.system(iverilog.format("-D tWHCONV_MIN=20_000")), NOT_FATAL)
+    self.assertEqual(os.system(vvp), NOT_FATAL)
 
-    self.assertEqual(os.system(iverilog.format("-DtWHCONV_MIN=35_000", "2")), NOT_FATAL)
-    self.assertNotEqual(os.system(vvp.format("2")), NOT_FATAL)
+    # tWHCONV == tWHCONV_MIN => test - PASS
+    self.assertEqual(os.system(iverilog.format("-D tWHCONV_MIN=30_000")), NOT_FATAL)
+    self.assertEqual(os.system(vvp), NOT_FATAL)
+
+    # tWHCONV < tWHCONV_MIN => test - FAIL
+    self.assertEqual(os.system(iverilog.format("-D tWHCONV_MIN=30_001")), NOT_FATAL)
+    self.assertNotEqual(os.system(vvp), NOT_FATAL)
 
   def test_tWHCONV_MAX(self):
-    self.assertEqual(os.system(iverilog.format("-DtWHCONV_MAX=40_000", "3")), NOT_FATAL)
-    self.assertEqual(os.system(vvp.format("3")), NOT_FATAL)
+    # in ltc2308_tb.v: tWHCONV = 30_000
 
-    self.assertEqual(os.system(iverilog.format("-DtWHCONV_MAX=30_000", "4")), NOT_FATAL)
-    self.assertEqual(os.system(vvp.format("4")), NOT_FATAL)
+    # tWHCONV < tWHCONV_MAX => test - PASS
+    self.assertEqual(os.system(iverilog.format("-D tWHCONV_MAX=40_000")), NOT_FATAL)
+    self.assertEqual(os.system(vvp), NOT_FATAL)
 
-    self.assertEqual(os.system(iverilog.format("-DtWHCONV_MAX=25_000", "5")), NOT_FATAL)
-    self.assertNotEqual(os.system(vvp.format("5")), NOT_FATAL)
+    # tWHCONV == tWHCONV_MAX => test - PASS
+    self.assertEqual(os.system(iverilog.format("-D tWHCONV_MAX=30_000")), NOT_FATAL)
+    self.assertEqual(os.system(vvp), NOT_FATAL)
+
+    # tWHCONV > tWHCONV_MAX => test - FAIL
+    self.assertEqual(os.system(iverilog.format("-D tWHCONV_MAX=29_999")), NOT_FATAL)
+    self.assertNotEqual(os.system(vvp), NOT_FATAL)
 
   @classmethod
   def tearDownClass(cls):
@@ -35,8 +60,8 @@ class ltc2308(unittest.TestCase):
       if (item.endswith(".vvp") or item.endswith(".vcd")) and (item.startswith("ltc2308_tb")):
         os.remove(os.path.join(dir_name, item))
 
-    os.system(iverilog.format("", ""))
-    os.system(vvp.format(""))
+    os.system(iverilog.format("-D get_vcd"))
+    os.system(vvp)
 
 if __name__ == '__main__':
   unittest.main()
